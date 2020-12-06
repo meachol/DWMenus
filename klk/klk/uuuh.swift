adc981610e8046b8d32233da20a7aa9cf21bb52a
meachol <362745046@qq.com>
2020-12-06 21:58:13 +0800

    vv

--- klk/klk/uuuh.swift
+++ klk/klk/uuuh.swift
@@ -9,12 +9,16 @@ import UIKit
 
 class uuuh: UIView {
 
-    /*
+
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        
+        
+    //    p
+    
     }
-    */
+    
 
 }
