20eb3ed6f5a4f3b77492c05b3e917bd1adccc13b
meachol <362745046@qq.com>
2020-12-06 22:07:06 +0800

    00

---
+++ klk/klk/fgsffdffgdf.swift
@@ -0,0 +1,26 @@
+//
+//  fgsffdffgdf.swift
+//  klk
+//
+//  Created by meachol on 2020/12/6.
+//
+
+import UIKit
+
+class fgsffdffgdf: UIView {
+
+  
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        print("dsvfzv")
+        print("dsvfzv")
+        print("dsvfzv")
+        print("dsvfzv")
+
+    }
+ 
+
+}
