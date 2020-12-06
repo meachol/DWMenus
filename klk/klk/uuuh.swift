3254e9076494882420ce6aaeca2d3a65ff76b0b1
meachol <362745046@qq.com>
2020-12-06 21:53:47 +0800

    jj

---
+++ klk/klk/uuuh.swift
@@ -0,0 +1,20 @@
+//
+//  uuuh.swift
+//  klk
+//
+//  Created by meachol on 2020/12/6.
+//
+
+import UIKit
+
+class uuuh: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
